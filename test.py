b743ba0ec6b75031217dfeada455657ab848cf2c
Anthony Novoa <anovoa@SEI.local>
2023-03-13 10:23:48 -0700

    added maximum smiles :)

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 
 
-print("this is a test")
-
+#print("this is a test")
+max_smiles = 10
 smiley = ":)"
+i = 0
 
-print(smiley)
+#print(smiley)
 
-print("THIS IS BRANCH TEST ONE")
+#print("THIS IS BRANCH TEST ONE")
+
+while i < max_smiles:
+
+    print(smiley, " ", end="")
+    i += 1
